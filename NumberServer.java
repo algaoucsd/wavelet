c4c9e9cbc49793dce96761769967da19e9e6e0b7
algaoucsd <algao@ucsd.edu>
2023-01-18 13:33:12 -0800

    Added Alex Gao's

--- NumberServer.java
+++ NumberServer.java
@@ -8,7 +8,7 @@ class Handler implements URLHandler {
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
+            return String.format("Alex Gao's Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -19,7 +19,7 @@ class Handler implements URLHandler {
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
+                }  
             }
             return "404 Not Found!";
         }
